4fdb3bbb251f5c2a410e879a4143494e6da9eace
Tham Saleerueng <44228788+tham9500@users.noreply.github.com>
2023-01-15 19:22:12 +0700

    Update Skill profile

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Gamer", "Developer"],
+        strings: ["Developer","Engineering Computer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Gamer", "Developer"],
+        strings: ["Developer","Engineering Computer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
